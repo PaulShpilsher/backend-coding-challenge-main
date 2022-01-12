823a62f3d2cc251f03e6ad46b258acf0ff68087e
Paul Shpilsher <paul.shpilsher@gmail.com>
2022-01-12 21:06:29 +0300

    insert into db

--- scripts/importData.ts
+++ scripts/importData.ts
@@ -1,6 +1,8 @@
+import { assert } from "console";
 import { readFileSync, stat } from "fs";
 import { join } from "path";
 import { Database } from "sqlite3";
+import { promisify } from "util";
 
 interface Organizer {
     name: string
@@ -14,8 +16,10 @@ interface Event {
     organizer: Organizer
 }
 
-export const importData = () => {
+const importData = () => {
     // Import the data in json format and save in database
+    const events: Event[] = loadEvents();
+
     const dbLocation = join(__dirname, '../data/myDb.db')
     const sqlite3 = require('sqlite3').verbose();
     const db: Database = new sqlite3.Database(dbLocation, err => {
@@ -26,6 +30,8 @@ export const importData = () => {
 
     try {
         createDbSchema(db);
+        
+        insertEvents(db, events);
     } finally {
         db.close(err => {
             if (err) {
@@ -34,12 +40,16 @@ export const importData = () => {
         });
     }
 
+
 }
 
+const readFile = (relativeFilePath: string): string => {
+    const filePath = join(__dirname, relativeFilePath);
+    return readFileSync(filePath, "utf-8");
+}
 
 const getSqlStatements = (): string[] => {
-    const sqlDataLocation = join(__dirname, "../data/tables.sql");
-    const dataSql = readFileSync(sqlDataLocation).toString();
+    const dataSql = readFile("../data/tables.sql");
     const statements = dataSql.toString().split(");");
     return statements.map(x=>x.trim()).filter(x=>!!x).map(x=>x+");");
 }
@@ -49,12 +59,6 @@ const createDbSchema = (db: Database): void => {
     if(!sqlStatements) {
         throw new Error("No SQL stetesment found")
     }
-    // db.run(sqlStatements[0], err => {
-    //     if (err) {
-    //         throw err;
-    //         }
-    //     });
-
     db.serialize(() => {
         db.run("BEGIN TRANSACTION;");
 
@@ -69,6 +73,49 @@ const createDbSchema = (db: Database): void => {
         
         db.run("COMMIT;");
     });
+}
+
+const loadEvents = (): Event[] => {
+    const s: string = readFile("../data/data.json");
+    return JSON.parse(s);
+}
+
+
+const insertEvents = (db: Database, events: Event[]) => {
+
+    db.serialize(() => {
+        events.forEach((event: Event) => {
+            db.run(
+                "INSERT INTO organizers(name) VALUES(?)",
+                [event.organizer.name], 
+                function(err) {
+                    //ignore
+                });
+
+            let organizerId;
+            db.get(
+                "SELECT id FROM organizers WHERE name = ?",
+                [event.organizer.name], 
+                (err, row) => {
+                    if (err) {
+                        throw err;
+                    }            
+                    
+                    console.log( row.id, row.name);
+                    organizerId = row.id;
+                });
+
+            db.run(
+                `INSERT INTO EVENTS(name, isOutside, location, date, organizer_id)
+                VALUES(?,?,?,?,?)`,
+                [event.name, event.isOutside, event.location, event.date, organizerId],
+                function(err) {
+                    if(err) {
+                        throw err;
+                    }
+                });
+            });
+        });
 }
 
 importData();
